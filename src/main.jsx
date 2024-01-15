27c20e78bc88cd05093fa1a646e186fee730b702
Wataru Hori <Bireleysaddiction@gmail.com>
2024-01-16 01:09:51 +0900

    Fixed createRoot as the deployed version didn't work

--- src/main.jsx
+++ src/main.jsx
@@ -2,4 +2,7 @@ import ReactDOM from "react-dom/client"
 import App from "./App.jsx"
 import "./index.css"
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />)
+const rootElement = document.getElementById("root")
+const root = ReactDOM.createRoot(rootElement)
+
+root.render(<App />)
