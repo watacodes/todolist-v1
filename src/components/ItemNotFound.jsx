ee044e2e5a66a0388e40d1b603587c76e87636a1
Wataru Hori <Bireleysaddiction@gmail.com>
2024-01-16 23:16:42 +0900

    Renamed components

--- src/components/ItemNotFound.jsx
+++
@@ -1,3 +0,0 @@
-export default function ItemNotFound() {
-  return <h2>No tasks added yet!</h2>
-}
--- src/components/Stats/Stats.jsx
+++
@@ -1,5 +0,0 @@
-export default function Stats() {
-  console.log("Rendering stats...")
-
-  return <h1>stats here</h1>
-}
